d75058ea8372fadd32b22cc336dbb7fda100de49
r1n0x <r1n0x-dev@proton.me>
2025-03-08 21:33:20 +0100

    tests: ValidationException, LabelGenerationException

--- src/breadcrumbs-bundle/tests/Unit/Exception/LabelGenerationExceptionTest.php
+++ src/breadcrumbs-bundle/tests/Unit/Exception/LabelGenerationExceptionTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace R1n0x\BreadcrumbsBundle\Tests\Unit\Exception;
 
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProviderExternal;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
@@ -13,7 +12,7 @@ use R1n0x\BreadcrumbsBundle\Exception\LabelGenerationException;
 use R1n0x\BreadcrumbsBundle\Internal\Model\BreadcrumbDefinition;
 use R1n0x\BreadcrumbsBundle\Internal\Model\RootBreadcrumbDefinition;
 use R1n0x\BreadcrumbsBundle\Internal\Model\RouteBreadcrumbDefinition;
-use R1n0x\BreadcrumbsBundle\Tests\DataProvider\Exception\LabelGenerationExceptionDataProvider;
+use R1n0x\BreadcrumbsBundle\Tests\Unused;
 
 /**
  * @author r1n0x <r1n0x-dev@proton.me>
@@ -27,14 +26,37 @@ use R1n0x\BreadcrumbsBundle\Tests\DataProvider\Exception\LabelGenerationExceptio
 class LabelGenerationExceptionTest extends TestCase
 {
     #[Test]
-    #[DataProviderExternal(LabelGenerationExceptionDataProvider::class, 'getMessageIsReadableTestScenarios')]
-    public function messageIsReadable(
-        BreadcrumbDefinition $definition,
-        int $expectedExceptionCode,
-        string $expectedMessage
-    ): void {
-        $exception = new LabelGenerationException($definition);
-        $this->assertEquals($expectedExceptionCode, $exception->getCode());
-        $this->assertEquals($expectedMessage, $exception->getMessage());
+    public function messageForRouteIsReadable(): void
+    {
+        $exception = new LabelGenerationException(new RouteBreadcrumbDefinition(
+            'route-bad7d22a-60f1-4982-adf4-a6d95ae17004',
+            'expression-b66783ae-f1bb-4c52-830a-85f9c152ffb5',
+            Unused::string(),
+            Unused::string(),
+            Unused::bool()
+        ));
+        $this->assertEquals(LabelGenerationException::CODE_ROUTE, $exception->getCode());
+        $this->assertEquals(sprintf(
+            'Error occurred when evaluating breadcrumb expression "%s" for route "%s"',
+            'expression-b66783ae-f1bb-4c52-830a-85f9c152ffb5',
+            'route-bad7d22a-60f1-4982-adf4-a6d95ae17004'
+        ), $exception->getMessage());
+    }
+
+    #[Test]
+    public function messageForRootIsReadable(): void
+    {
+        $exception = new LabelGenerationException(new RootBreadcrumbDefinition(
+            Unused::string(),
+            'expression-71215cb4-d74a-47b4-96ad-0b21aa614cb6',
+            'name-2486b414-6c92-4d50-bbde-05e42337e6d7',
+            Unused::array()
+        ));
+        $this->assertEquals(LabelGenerationException::CODE_ROOT, $exception->getCode());
+        $this->assertEquals(sprintf(
+            'Error occurred when evaluating breadcrumb expression "%s" for root "%s"',
+            'expression-71215cb4-d74a-47b4-96ad-0b21aa614cb6',
+            'name-2486b414-6c92-4d50-bbde-05e42337e6d7'
+        ), $exception->getMessage());
     }
 }
--- src/breadcrumbs-bundle/tests/Unit/Exception/ValidationExceptionTest.php
+++ src/breadcrumbs-bundle/tests/Unit/Exception/ValidationExceptionTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace R1n0x\BreadcrumbsBundle\Tests\Unit\Exception;
 
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProviderExternal;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
@@ -14,7 +13,6 @@ use R1n0x\BreadcrumbsBundle\Internal\Model\Violation\Error;
 use R1n0x\BreadcrumbsBundle\Internal\Model\Violation\RootError;
 use R1n0x\BreadcrumbsBundle\Internal\Model\Violation\RouteError;
 use R1n0x\BreadcrumbsBundle\Internal\Validator\Node\ValidationContext;
-use R1n0x\BreadcrumbsBundle\Tests\DataProvider\Exception\ValidationExceptionDataProvider;
 
 /**
  * @author r1n0x <r1n0x-dev@proton.me>
@@ -29,12 +27,89 @@ use R1n0x\BreadcrumbsBundle\Tests\DataProvider\Exception\ValidationExceptionData
 class ValidationExceptionTest extends TestCase
 {
     #[Test]
-    #[DataProviderExternal(ValidationExceptionDataProvider::class, 'getMessageIsReadableTestScenarios')]
-    public function messageIsReadable(
-        ValidationContext $context,
-        string $expectedMessage
-    ): void {
+    public function messageForRouteParametersIsReadable(): void
+    {
+        $context = new ValidationContext();
+        $context
+            ->addRouteParameterViolation(
+                'route-b7ca076b-1b03-4217-864e-b9b2fb188aa6',
+                'parameter-7ed7fd91-c7c1-4890-9122-fbf83de0b6f4'
+            );
+        $context
+            ->addRouteParameterViolation(
+                'route-b7ca076b-1b03-4217-864e-b9b2fb188aa6',
+                'parameter-00ff7b4e-ce81-4595-a458-f850c46202eb'
+            );
+        $context
+            ->addRouteParameterViolation(
+                'route-b2302df9-69e7-4105-b63c-521f1a13efea',
+                'parameter-50cd1bd9-4862-4cdc-83eb-6422ebf4562b'
+            );
+
         $exception = new ValidationException($context);
-        $this->assertEquals($expectedMessage, $exception->getMessage());
+        $this->assertEquals(<<<'TEXT'
+Breadcrumb validation failed:
+Parameters [parameter-7ed7fd91-c7c1-4890-9122-fbf83de0b6f4, parameter-00ff7b4e-ce81-4595-a458-f850c46202eb] required by route "route-b7ca076b-1b03-4217-864e-b9b2fb188aa6" were not set.
+Parameters [parameter-50cd1bd9-4862-4cdc-83eb-6422ebf4562b] required by route "route-b2302df9-69e7-4105-b63c-521f1a13efea" were not set.
+
+TEXT, $exception->getMessage());
+    }
+
+    #[Test]
+    public function messageForRouteVariablesIsReadable(): void
+    {
+        $context = new ValidationContext();
+        $context
+            ->addRouteVariableViolation(
+                'route-b7ca076b-f3235298-c3b3-4579-bd55-6f769ff035d7',
+                'variable-67829435-998c-4c73-adda-ff5b43c6fdad'
+            );
+        $context
+            ->addRouteVariableViolation(
+                'route-b7ca076b-f3235298-c3b3-4579-bd55-6f769ff035d7',
+                'variable-0d7d204d-fa4d-40e3-869a-ffbce8e659d9'
+            );
+        $context
+            ->addRouteVariableViolation(
+                'route-5270c8cc-b4bd-4f3b-a94a-06561692ffc0',
+                'variable-e1620779-b947-47a9-b9c5-e3050afd8b60'
+            );
+
+        $exception = new ValidationException($context);
+        $this->assertEquals(<<<'TEXT'
+Breadcrumb validation failed:
+Variables [variable-67829435-998c-4c73-adda-ff5b43c6fdad, variable-0d7d204d-fa4d-40e3-869a-ffbce8e659d9] required by route "route-b7ca076b-f3235298-c3b3-4579-bd55-6f769ff035d7" were not set.
+Variables [variable-e1620779-b947-47a9-b9c5-e3050afd8b60] required by route "route-5270c8cc-b4bd-4f3b-a94a-06561692ffc0" were not set.
+
+TEXT, $exception->getMessage());
+    }
+
+    #[Test]
+    public function messageForRootIsReadable(): void
+    {
+        $context = new ValidationContext();
+        $context
+            ->addRootVariableViolation(
+                'route-cf6712d0-f6f9-4dc0-afdc-1bf7b4ea29b4',
+                'variable-a8e82b30-3fe7-4e70-be77-120522758c73'
+            );
+        $context
+            ->addRootVariableViolation(
+                'route-cf6712d0-f6f9-4dc0-afdc-1bf7b4ea29b4',
+                'variable-da2a5fdf-44f9-40c4-b5c1-7619f4eabe62'
+            );
+        $context
+            ->addRootVariableViolation(
+                'route-3c2d7a76-d67a-45c3-8725-adf9ec4c7d83',
+                'variable-baa1a12d-d92c-4492-a740-815dc17bfa53'
+            );
+
+        $exception = new ValidationException($context);
+        $this->assertEquals(<<<'TEXT'
+Breadcrumb validation failed:
+Variables [variable-a8e82b30-3fe7-4e70-be77-120522758c73, variable-da2a5fdf-44f9-40c4-b5c1-7619f4eabe62] required by root "route-cf6712d0-f6f9-4dc0-afdc-1bf7b4ea29b4" were not set.
+Variables [variable-baa1a12d-d92c-4492-a740-815dc17bfa53] required by root "route-3c2d7a76-d67a-45c3-8725-adf9ec4c7d83" were not set.
+
+TEXT, $exception->getMessage());
     }
 }
